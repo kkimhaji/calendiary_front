c7dffc0efd66063ac8c991b39f7670feb4c6cd6a
DESKTOP-MV6SI2A\hjh <kkim.haji@gmail.com>
2025-02-19 19:14:17 +0900

    [feat] delete post edit permission

--- src/constants/CategoryPermission.js
+++ src/constants/CategoryPermission.js
@@ -9,25 +9,20 @@ export const CategoryPermision = {
         label: '게시글 작성',
         position: 1
     },
-    EDIT_POST: {
-        key: 'EDIT_POST',
-        label: '게시글 수정',
-        position: 2
-    },
     DELETE_POST:{
         key: 'DELETE_POST',
         label: '게시글 삭제',
-        position: 3
+        position: 2
     },
     CREATE_COMMENT:{
         key: 'CREATE_COMMENT',
         label: '댓글 생성',
-        position: 4
+        position: 3
     },
     DELETE_COMMENT:{
         key: 'DELETE_COMMENT',
         label: '댓글 삭제',
-        position: 5
+        position: 4
     }
 };
 
