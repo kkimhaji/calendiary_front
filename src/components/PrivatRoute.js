65f2a7203e264f951d283eee38f87d70aec6f654
kkimhaji <kkim.haji@gmail.com>
2026-05-30 20:10:27 +0900

    [fix] remove non-existent selectIsTeamMember import from authSlice

--- src/components/PrivatRoute.js
+++ src/components/PrivatRoute.js
@@ -1,18 +1,22 @@
-import React, { useEffect } from 'react';
-import { Navigate, useParams, useLocation } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectIsAuthenticated, selectIsTeamMember } from '../store/authSlice';
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectIsAuthenticated } from '../store/authSlice';
 
 const PrivateRoute = ({ children }) => {
     const isAuthenticated = useSelector(selectIsAuthenticated);
     const location = useLocation();
-    
+
     if (!isAuthenticated) {
-      // 현재 URL을 쿼리 파라미터로 포함시켜 로그인 페이지로 리다이렉트
-      return <Navigate to={`/login?redirectUrl=${encodeURIComponent(location.pathname + location.search)}`} replace />;
+        return (
+            <Navigate
+                to={`/login?redirectUrl=${encodeURIComponent(location.pathname + location.search)}`}
+                replace
+            />
+        );
     }
-    
+
     return children;
-  };
-  
-  export default PrivateRoute;
+};
+
+export default PrivateRoute;
